bed7c49d73fcf46faa1fb84a368cb444ddb41e27
agentduck046-coder <agentduck046@gmail.com>
2026-03-16 01:13:42 -0400

    Update background.tsx

--- components/background.tsx
+++ components/background.tsx
@@ -1,11 +1,7 @@
 "use client"
 
-export default function Background(){
-
-return(
-
-<div className="fixed inset-0 -z-10 bg-gradient-to-br from-black via-neutral-900 to-black"/>
-
-)
-
+export default function Background() {
+  return (
+    <div className="fixed inset-0 -z-10 bg-gradient-to-br from-indigo-900 via-purple-900 to-black opacity-40" />
+  )
 }
