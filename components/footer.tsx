0b5143a6507905db9865218a9db50ce7fc3361f6
agentduck046-coder <agentduck046@gmail.com>
2026-03-15 23:41:50 -0400

    Add Footer component with copyright notice

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer(){
+
+return(
+
+<footer className="border-t border-neutral-800 mt-24 py-10 text-center text-gray-500">
+
+<p>
+© {new Date().getFullYear()} Adrift Works
+</p>
+
+</footer>
+
+)
+
+}
