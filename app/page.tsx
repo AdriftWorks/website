7d629376c76dc436f9f846adafd45705e9064a54
agentduck046-coder <agentduck046@gmail.com>
2026-03-20 21:37:19 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 import Hero from "@/components/hero"
 import IdentityStrip from "@/components/IdentityStrip"
+import Capabilities from "@/components/sections/Capabilities"
+import ToolHighlight from "@/components/sections/ToolHighlight"
+import CTA from "@/components/sections/CTA"
 import BackgroundAnimation from "@/components/BackgroundAnimation"
 
 export default function Home() {
   return (
-    <main className="relative bg-black text-white min-h-screen overflow-hidden">
+    <main className="relative bg-black text-white min-h-screen">
 
-      {/* Background */}
       <BackgroundAnimation />
 
-      {/* Content */}
       <Hero />
       <IdentityStrip />
+      <Capabilities />
+      <ToolHighlight />
+      <CTA />
 
     </main>
-  );
+  )
 }
