cacc4de656cdcc0f9977e073336db49376236222
agentduck046-coder <agentduck046@gmail.com>
2026-03-20 22:06:00 -0400

    Create page.tsx

---
+++ app/tools/Workflow-Generator/page.tsx
@@ -0,0 +1,9 @@
+import WorkflowGenerator from "@/components/WorkflowGenerator"
+
+export default function ToolsPage() {
+  return (
+    <main className="bg-black text-white min-h-screen">
+      <WorkflowGenerator />
+    </main>
+  )
+}
