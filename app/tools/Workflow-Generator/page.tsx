341857a8178d64726bba0dea3bd05b9494ed5027
agentduck046-coder <agentduck046@gmail.com>
2026-03-20 22:13:38 -0400

    Update page.tsx

--- app/tools/Workflow-Generator/page.tsx
+++ app/tools/Workflow-Generator/page.tsx
@@ -1,9 +1,11 @@
 import WorkflowGenerator from "@/components/WorkflowGenerator"
 
-export default function ToolsPage() {
+export default function WorkflowGeneratorPage() {
   return (
-    <main className="bg-black text-white min-h-screen">
-      <WorkflowGenerator />
+    <main className="bg-black text-white min-h-screen px-6 py-20">
+      <div className="max-w-4xl mx-auto">
+        <WorkflowGenerator />
+      </div>
     </main>
   )
 }
